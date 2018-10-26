c1acc51b2a5508aee05906fb2c7d5255c01c0d96
Sam Leech <marwoodyt@gmail.com>
2018-10-26 08:28:06 +0100

    Imporved the layout of my HTML CSS and JS

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs= require ('fs');
+const fs = require('fs');
 
 
 const app = express()
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
@@ -37,60 +39,54 @@ app.get('/editgpu', (req, res) => {
     res.render('edit/editgpu')
 })
 
-app.post('/add/gpu',(req,res) =>{
-    var Stock = require ("./appdata/stock")
-    Stock.cards.push (
-        {
-            Amount:req.body.Amount,
-            Name:req.body.Name,
-            Price:req.body.Price,
-            ID:req.body.ID
-        }
-    )
+app.post('/add/gpu', (req, res) => {
+    var Stock = require("./appdata/stock")
+    Stock.cards.push({
+        Amount: req.body.Amount,
+        Name: req.body.Name,
+        Price: req.body.Price,
+        ID: req.body.ID
+    })
     fs.writeFile(
         "appdata/stock.json",
         JSON.stringify(Stock),
-        )
+    )
     res.render('add/gpu')
 })
 
-app.post('/add/cpu',(req,res) =>{
-    var Stock = require ("./appdata/stock")
-    Stock.cpu.push (
-        {
-            Amount:req.body.Amount,
-            Name:req.body.Name,
-            Price:req.body.Price,
-            ID:req.body.ID
-        }
-    )
+app.post('/add/cpu', (req, res) => {
+    var Stock = require("./appdata/stock")
+    Stock.cpu.push({
+        Amount: req.body.Amount,
+        Name: req.body.Name,
+        Price: req.body.Price,
+        ID: req.body.ID
+    })
     fs.writeFile(
         "appdata/stock.json",
         JSON.stringify(Stock),
-        )
+    )
     res.render('add/cpu')
 })
 
 
-app.post('/add/cases',(req,res) =>{
-    var Stock = require ("./appdata/stock")
-    Stock.cases.push (
-        {
-            Amount:req.body.Amount,
-            Name:req.body.Name,
-            Price:req.body.Price,
-            ID:req.body.ID
-        }
-    )
+app.post('/add/cases', (req, res) => {
+    var Stock = require("./appdata/stock")
+    Stock.cases.push({
+        Amount: req.body.Amount,
+        Name: req.body.Name,
+        Price: req.body.Price,
+        ID: req.body.ID
+    })
     fs.writeFile(
         "appdata/stock.json",
         JSON.stringify(Stock),
-        )
+    )
     res.render('add/cases')
 })
 
 app.get('/shop', (req, res) => {
-   var Stock = require ("./appdata/stock")
+    var Stock = require("./appdata/stock")
     res.render('shop', {
         gpu: Stock.cards,
         cpu: Stock.cpu,
@@ -102,6 +98,4 @@ app.get('/shop', (req, res) => {
 
 app.listen(3000, function () {
     console.log('HOSTING_ON_PORT_3000')
-  })
-
-  
+})
